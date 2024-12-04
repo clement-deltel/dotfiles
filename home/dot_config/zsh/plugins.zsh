92d0e894e0900ae10f5e42b5db363caefbf7829f
Clement Deltel <38040315+clement-deltel@users.noreply.github.com>
2024-12-03 22:39:11 -0600

    build: switch from antigen to antidote

---
+++ home/dot_config/zsh/plugins.zsh
@@ -0,0 +1,68 @@
+# Load oh-my-zsh library
+getantidote/use-omz
+ohmyzsh/ohmyzsh path:lib
+
+# Load bundles from the default repo (oh-my-zsh)
+# Aliases
+ohmyzsh/ohmyzsh path:plugins/aliases
+ohmyzsh/ohmyzsh path:plugins/alias-finder
+ohmyzsh/ohmyzsh path:plugins/common-aliases
+
+# Cloud
+ohmyzsh/ohmyzsh path:plugins/aws
+
+# Configuration Management
+ohmyzsh/ohmyzsh path:plugins/ansible
+ohmyzsh/ohmyzsh path:plugins/chezmoi
+
+# Distro-related
+ohmyzsh/ohmyzsh path:plugins/ubuntu
+
+# Infrastructure as Code
+ohmyzsh/ohmyzsh path:plugins/terraform
+
+# Languages - Go
+ohmyzsh/ohmyzsh path:plugins/golang
+
+# Languages - JavaScript
+ohmyzsh/ohmyzsh path:plugins/node
+
+# Languages - Python
+ohmyzsh/ohmyzsh path:plugins/pip
+ohmyzsh/ohmyzsh path:plugins/poetry
+
+# Packages
+ohmyzsh/ohmyzsh path:plugins/brew
+ohmyzsh/ohmyzsh path:plugins/npm
+
+# Productivity
+ohmyzsh/ohmyzsh path:plugins/colored-man-pages
+ohmyzsh/ohmyzsh path:plugins/colorize
+ohmyzsh/ohmyzsh path:plugins/command-not-found
+ohmyzsh/ohmyzsh path:plugins/safe-paste
+
+# Orchestration
+ohmyzsh/ohmyzsh path:plugins/docker
+ohmyzsh/ohmyzsh path:plugins/docker-compose
+ohmyzsh/ohmyzsh path:plugins/helm
+ohmyzsh/ohmyzsh path:plugins/kubectl
+
+# Shell
+ohmyzsh/ohmyzsh path:plugins/cp
+ohmyzsh/ohmyzsh path:plugins/eza
+ohmyzsh/ohmyzsh path:plugins/fzf
+ohmyzsh/ohmyzsh path:plugins/history
+ohmyzsh/ohmyzsh path:plugins/sudo
+
+# Version Control System
+ohmyzsh/ohmyzsh path:plugins/git
+ohmyzsh/ohmyzsh path:plugins/git-lfs
+ohmyzsh/ohmyzsh path:plugins/pre-commit
+
+# Other
+ohmyzsh/ohmyzsh path:plugins/vscode
+
+# Load bundles from external repos
+zsh-users/zsh-autosuggestions
+zsh-users/zsh-completions
+zsh-users/zsh-syntax-highlighting

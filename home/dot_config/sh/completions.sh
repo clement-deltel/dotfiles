62b89f45377c1fd5c4dc0bfdd0be129973610e1f
Clement Deltel <38040315+clement-deltel@users.noreply.github.com>
2025-10-14 15:18:10 -0500

    feat: review completions and zsh plugins

--- home/dot_config/sh/completions.sh
+++ home/dot_config/sh/completions.sh
@@ -3,22 +3,37 @@
 # Configuration Management - Chezmoi
 eval "$(chezmoi completion bash)"
 
-# Infrastructure as Code
+# Cloud
+complete -C '/usr/local/bin/aws_completer' aws
+
+# Infrastructure as Code (IaC)
+eval "$(pulumi gen-completion bash)"
 eval "$(terraform -install-autocomplete)"
 
 # Language - Python
 eval "$(poetry completions bash)"
+eval "$(register-python-argcomplete cz)"
+eval "$(register-python-argcomplete pipx)"
 eval "$(uv generate-shell-completion bash)"
 eval "$(uvx --generate-shell-completion bash)"
+source ${HOME}/.pyenv/completions/pyenv.bash
 
 # Orchestration
 eval "$(docker completion bash)"
+eval "$(grype completion bash)"
 eval "$(helm completion bash)"
+eval "$(helmfile completion bash)"
 eval "$(kubectl completion bash)"
+eval "$(stern --completion bash)"
+eval "$(trivy completion bash)"
 
-# Shell
+# Tools
 eval "$(fzf --bash)"
+eval "$(gonzo completion bash)"
+eval "$(procs --gen-completion-out bash)"
+# eval "$(rg --generate complete-bash)" # TODO: not working
+eval "$(yq shell-completion bash)"
 
-# Version Control
+# Version Control System (VCS)
 source /usr/share/bash-completion/completions/git
 eval "$(gitleaks completion bash)"
